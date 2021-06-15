93e142a1296a93c0b622d0f60b6f9130cbd95ddc
ayushbisht2001 <bisht.sanu2001@gmail.com>
2021-06-15 04:59:06 -0700

    fix issue

--- src/Api/server.js
+++ src/Api/server.js
@@ -1,4 +1,4 @@
-const server = 'http://127.0.0.1:8000';
-// const server =  "https://nemesisapi.herokuapp.com";
+// const server = 'http://127.0.0.1:8000';
+const server =  "https://nemesisapi.herokuapp.com";
 
 export default server;
